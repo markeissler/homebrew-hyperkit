f8a72c185a1358ecf0aeb1ee65328df2852593c1
Mark Eissler <mark@mixtur.com>
2017-07-08 00:33:09 -0700

    HHYP-10 #comment #time 8h Refactor for PR feedback.

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -1,70 +1,11 @@
 class Hyperkit < Formula
-  require "open3"
-
-  desc "Lightweight virtualization hypervisor for MacOS"
+  desc "Lightweight virtualization hypervisor for macOS"
   homepage "https://github.com/moby/hyperkit"
+  url "https://github.com/moby/hyperkit.git",
+    :tag => "v0.20170425",
+    :revision => "a9c368bed6003bee11d2cf646ed1dcf3d350ec8c"
 
-  # Retrieve version and commit hash from local git repo
-  #
-  # The tip of the specified branch will be examined to generate a version that
-  # is consistent with a bonafide tagged release version string but where the
-  # major version number is "HEAD" and the commit hash is simply the associated
-  # commit hash for the tip of the branch.
-  #
-  # @example Example generated version string
-  #   vHEAD.20170425
-  #
-  # @param [String] build_path repo directory
-  # @param branch [String] target branch
-  #
-  # @return [Array] array containing version in field 1, commit hash in field 2
-  #
-  def self.version_from_git(build_path, branch = "master")
-    command = <<-CMD.undent
-      \\cd "#{build_path}"; \
-      \\git log -1 --pretty=format:"vHEAD.%cd-%h" --date=short #{branch}
-    CMD
-    version_string, _stderr, _status = Open3.capture3(command.chomp)
-    version_string.split("-", 3).join("").split("-")
-  end
-
-  # Retrieve version and commit hash from Resource object
-  #
-  # The resource must have the :tag and :revision fields defined in its specs
-  # attribute.
-  #
-  # @param [Resource] resource target resource
-  #
-  # @return [Array] array containing version in field 1, commit hash in field 2
-  #
-  def self.version_from_resource(resource)
-    if !resource.specs.key?(:tag) || resource.specs[:tag].to_s.empty?
-      odie "Couldn't figure out version from resource!"
-    end
-    if !resource.specs.key?(:revision) || resource.specs[:revision].to_s.empty?
-      odie "Couldn't figure out commit hash from resource!"
-    end
-    [resource.specs[:tag][0..-1], resource.specs[:revision][0..6]]
-  end
-
-  # Parse version and commit hash form url
-  #
-  # Url must be in the following format:
-  #   https://dl.bintray.com/markeissler/homebrew/hyperkit/hyperkit-v0.20170515-fa78d94.tar.gz
-  #
-  # @param [String] url url to parse
-  #
-  # @return [Array] array containing version in field 1, commit hash in field 2
-  #
-  def self.version_from_url(url)
-    url.scan(/hyperkit-(v[\d]{1}.[\d]{8})-([A-Fa-f\d]+).tar.gz$/).first
-  end
-
-  stable do
-    url "https://github.com/moby/hyperkit.git",
-      :tag => "v0.20170425",
-      :revision => "a9c368bed6003bee11d2cf646ed1dcf3d350ec8c"
-  end
+  head "https://github.com/moby/hyperkit.git"
 
   bottle do
     root_url "http://dl.bintray.com/markeissler/homebrew/bottles"
@@ -74,12 +15,10 @@ class Hyperkit < Formula
     sha256 "0fb4cf0f9f8d81eb1be99d620b454ab14ec8680237f6938db095b032874e821d" => :yosemite
   end
 
-  head do
-    url "https://github.com/moby/hyperkit.git", :branch => "master"
-  end
-
-  depends_on "opam" => :run
-  depends_on "libev" => :run
+  depends_on "ocaml" => :build
+  depends_on "opam" => :build
+  depends_on "aspcud" => :build
+  depends_on "libev" => :build
 
   resource "tinycorelinux" do
     url "https://dl.bintray.com/markeissler/homebrew/hyperkit-kernel/tinycorelinux_8.x.tar.gz"
@@ -87,28 +26,37 @@ class Hyperkit < Formula
   end
 
   def install
-    ohai "... Installing hyperkit dependencies with OPAM. This might take a while."
+    ENV["OPAMROOT"] = buildpath/"opamroot"
+    ENV["OPAMYES"] = "1"
+    system "opam", "init", "--no-setup"
+    system "opam", "install", "uri", "qcow.0.10.0", "qcow-tool",
+                   "mirage-block-unix.2.7.0", "conf-libev", "logs", "fmt",
+                   "mirage-unix", "prometheus-app"
 
-    system <<-CMD.undent
-      export OPAMYES=1
-      opam init
-      eval "$(opam config env)"
-      opam install uri qcow.0.10.0 qcow-tool mirage-block-unix.2.7.0 conf-libev logs fmt mirage-unix prometheus-app
-    CMD
-
-    ohai "... Dependencies installed."
-
-    # update the Makefile to set version to X.YYYYmmdd-sha1
+    # update the Makefile to set version to X.YYYYmmdd (sha1)
     if build.head?
-      version, sha1 = Hyperkit.version_from_git(buildpath)
+      command = <<-CMD.undent
+        \\cd "#{buildpath}"; \
+        \\git log -1 --pretty=format:"vHEAD.%cd-%h" --date=short "master"
+      CMD
+      version_string = Utils.popen_read(command.to_s).chomp
+      version, sha1 = version_string.split("-", 3).join("").split("-")
     else
-      # no need to re-parse version, we already set it in stable declaration above
-      version, sha1 = Hyperkit.version_from_resource(stable)
+      # grab version and sha1 from stable resource specs
+      version = stable.specs[:tag][0..-1]
+      sha1 = stable.specs[:revision][0..6]
     end
+
     if version.nil? || version.empty? || sha1.nil? || sha1.empty?
       odie "Couldn't figure out which version we're building!"
     end
-    update_makefile(buildpath, version, sha1)
+
+    quiet_system <<-CMD.undent
+      \\sed -i".bak" \
+      -e "s/GIT_VERSION[\ ]*:=.*/GIT_VERSION := '#{version} (#{sha1})'/g" \
+      -e "s/GIT_VERSION_SHA1[\ ]:=.*/GIT_VERSION_SHA1 := '#{sha1}'/g" \
+      "#{buildpath}/Makefile"
+    CMD
 
     system "make"
 
@@ -117,11 +65,12 @@ class Hyperkit < Formula
   end
 
   test do
-    #
-    # Download tinycorelinux kernel and initrd, boot system, check for prompt.
-    #
-    ohai "... Running tests."
+    # simple test when not in a vm that supports guests (i.e. VT-x disabled)
+    unless Hardware::CPU.features.include? :vmx
+      return system bin/"hyperkit", "-version"
+    end
 
+    # download tinycorelinux kernel and initrd, boot system, check for prompt
     resource("tinycorelinux").stage do |context|
       tmpdir = context.staging.tmpdir
       path_resource_versioned = Dir.glob(tmpdir.join("tinycorelinux_[0-9]*"))[0]
@@ -129,8 +78,7 @@ class Hyperkit < Formula
       cp(File.join(path_resource_versioned, "initrd.gz"), testpath)
     end
 
-    # boot tinycorelinux and check for a prompt
-    (testpath/"test_hyperkit.exp").write strip_heredoc(<<-EOS)
+    (testpath/"test_hyperkit.exp").write <<-EOS.undent
       #!/usr/bin/env expect -d
 
       set KERNEL "./vmlinuz"
@@ -163,24 +111,6 @@ class Hyperkit < Formula
 
       puts "\\nPASS"
     EOS
-
     system "expect", "test_hyperkit.exp"
-  end
-
-  private
-
-  # A more flexible version of undent, compress removes newlines
-  def strip_heredoc(text, compress = false)
-    stripped = text.gsub(/^#{text.scan(/^\s*/).min_by(&:length)}/, "")
-    compress ? stripped.tr("\n", " ").chop : stripped
-  end
-
-  def update_makefile(build_path, version, sha1)
-    system strip_heredoc(<<-CMD, true)
-      \\sed -i".bak"
-      -e "s/GIT_VERSION[\ ]*:=.*/GIT_VERSION := '#{version} (#{sha1})'/g"
-      -e "s/GIT_VERSION_SHA1[\ ]:=.*/GIT_VERSION_SHA1 := '#{sha1}'/g"
-      "#{build_path}/Makefile"
-    CMD
   end
 end
